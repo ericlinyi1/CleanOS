861f190b036db3291aff8629a306d76795c83920
Eric <ericlinyi@gmail.com>
2026-03-14 18:34:55 +0800

    Initial Commit

---
+++ CleanOSTests/CleanOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanOSTests.swift
+//  CleanOSTests
+//
+//  Created by Eric Lin on 14/3/26.
+//
+
+import Testing
+@testable import CleanOS
+
+struct CleanOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
